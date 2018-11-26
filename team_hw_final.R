1e53aae8569d655a061360a413188896cecc6ef6
GomgomKim <dhflwlsjfzz@naver.com>
2018-11-26 14:44:54 +0900

    부정어 추출 추가했습니다.

--- team_hw_final.R
+++ team_hw_final.R
@@ -56,35 +56,92 @@ MK_RAW <- function(n){
 new_n <- MK_RAW(1500)
 View(new_n)
 
-nwd <- c(1, NC(df$content[1]))
-wd_list <- paste(nwd, collapse =',')
-wd_list2 <- paste(df$content[2], collapse=',')
-wd_list <- rbind(wd_list, wd_list2)
-wd_list
+# nwd <- c(1, NC(df$content[1]))
+# wd_list <- paste(nwd, collapse =',')
+# wd_list2 <- paste(df$content[2], collapse=',')
+# wd_list <- rbind(wd_list, wd_list2)
+# wd_list
 
-sum(is.na(df$content))
-NC(df$content[4500])
+# sum(is.na(df$content))
+# NC(df$content[4500])
+nwd <- c(1, NC(df$content[1]))
+wd_list_add <- paste(nwd, collapse = ',')
+wd_list_add <- c(wd_list_add, "불량")
+wd_list_add < t(wd_list_add)
+wd_list_add
+wd <- c("불량")
+wd <- c(wd, "갑질")
+wd
+wdwd <- paste(wd, collapse = ',')
+wdwd
+wd_list_add <- c(wd_list_add, wdwd)
+wd_list_add
+bad_keyword <- c()
+bad_keyword
 
 # 1 ~ n행까지 데이터셋 만들기
-MK_wd_list <- function(n){
+# flag => 0: 전체, 1: 부정 데이터 생성 
+MK_wd_list <- function(n, flag){
+  # 초기화 
   nwd <- c(1, NC(df$content[1]))
   wd_list <- paste(nwd, collapse =',')
+  bad_wd_list <- cbind(wd_list, "부정")
+  
+  # 부정어
+  bad_wd <- c("불매", "갑질", "사태", "불량", "소송", "불공정", "이물질", "논란", "구설수", "벌레", "과징금")
+  
   for(i in 2:n){
+    # na인건 패스 
     if(is.na(df$content[i])){
       next;
     }
+    
+    # 추가할 line 
     nwd_add <- c(i, NC(df$content[i]))
     wd_list_add <- paste(nwd_add, collapse = ',')
-    wd_list <- rbind(wd_list, wd_list_add)
+    
+    #### 부정
+    if(flag == 1){  
+      bad_keyword <- c() # 이 게시물에 포함된 부정어 리스트 
+      for(k in 1:length(nwd_add)){
+        for(j in 1:length(bad_wd)){
+          if(nwd_add[k] == bad_wd[j]){ # 부정 단어가 포함돼 있다면 
+            bad_keyword <- c(bad_keyword, bad_wd[j])
+          }
+        }
+      }
+      if(is.null(bad_keyword)){ # 부정단어 안나왔으면 통과
+        next; 
+      } else{ # 나왔으면 걸렸던 부정단어들과 함께 저장
+        bad_keywords <- paste(bad_keyword, collapse = ',')
+        bad_wd_list_add <- cbind(wd_list_add, bad_keywords)
+        bad_wd_list <- rbind(bad_wd_list, bad_wd_list_add)
+      }
+      
+    } 
+    #### 전체
+    else if (flag == 0){
+      wd_list <- rbind(wd_list, wd_list_add)
+    }
   }
-  return(wd_list)
+  
+  if(flag == 0){
+    return(wd_list)
+  } else if(flag == 1){  
+    return(bad_wd_list)
+  } 
+  
 }
 
-wd_lists <- MK_wd_list(2000)
+# 1500개의 전체 데이터 
+raw_data <- MK_wd_list(1500, 0)
+View(raw_data)
+# 4500개 중에 부정 데이터 추출 
+meta_data <- MK_wd_list(1500, 1)
+View(meta_data)
 
 
-
-
+####################################
 
 unique(unlist(strsplit(PosiNega(posi[1:100]), " ")))
 
