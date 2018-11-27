89e1d99fea8ad6b771503ce34e3897d391b329a9
theorist17 <theorist17@gmail.com>
2018-11-27 21:24:31 +0900

    basket formatting

--- Data_Refiner.R
+++ Data_Refiner.R
@@ -1,103 +1,89 @@
-library(readxl)
+#install.packages("stringr")
+#install.packages("readxl")
+#install.packages("KoNLP") 
+
+
+################ environment #############
 setwd("/Users/theorist/Documents/R-WorkingDirectory/DataSentiment")
-df <- read_excel("NamYang_Data_Refined.xlsx",col_names=T,na="NA")
-df <- df[rowSums(sapply(df[,11], is.na) == 0) > 0, ]
-colSums(sapply(df, is.na))
 
-View(df)
-library(KoNLP)
-
-#build dic
-dics <- c('woorimalsam') #sejong ...
+library(KoNLP) # Korean natural language processor
+#dictionary buildup
+dics <- c('woorimalsam', 'sejong') #sejong ...
 category <- c('emotions')
-user_d <- data.frame(readLines("wenoun.txt"), "ncn") #fill this file
+user_d <- data.frame(readLines("wenoun.txt"), "ncn") # help NLP improve
 buildDictionary(ext_dic = dics,
                 category_dic_nms = category, 
                 user_dic = user_d,
                 replace_usr_dic = F,)
 
-#extraction for noun
-#install.packages("stringr")
-library(stringr)
-user_werm <- data.frame(readLines("werm.txt"), "ncn")
-doc2 <- paste(SimplePos22(df$content[1]))
-doc3 <- str_match(doc2, "([가-힣]+)/NC")
-doc3
-?str_match
-doc4 <- doc3[,2]
-doc4
-doc4[!is.na(doc4)]
+##################### data loading ######################
+library(readxl)
+df <- read_excel("NamYang_Data_Refined.xlsx",col_names=T,na="NA")
+df <- df[rowSums(sapply(df[,11], is.na) == 0) > 0, ]
+colSums(sapply(df, is.na))
+View(df)
+df.bkup <- df
 
-NC <- function(doc){
-  #doc <- as.character(doc)
-  doc2 <- paste(SimplePos22(doc))
-  doc3 <- str_match(doc2, "([가-힣]+)/NC")
-  doc4 <- doc3[,2]
-  doc4[!is.na(doc4)]
-  return(doc4[!is.na(doc4)])
+################### elimination 수정 필요 ######################
+str_replace_all(df$content, "[ㄱ-ㅎ]", "") %>%  #remove ㅋㅋㅋ
+  str_replace_all("[0-9]", "") %>%              #remove number
+  str_replace_all("[[:punct:]]", "")            #remove punctuation
+
+
+# test <- "ㅋㅋㅋ ㅎㅎㅎ 1월 2일 ^^ ;; @@ :)"
+# str_replace_all(test, "[ㄱ-ㅎ]", "") %>%  #remove ㅋㅋㅋ
+#   str_replace_all("[0-9]", "") %>%              #remove number
+#   str_replace_all("[[:punct:]]", "")            #remove punctuation
+###################### pos tagging ##########################
+textToBasket <- function(text){
+  taggedText <- paste(MorphAnalyzer(text))
+  attrs <- str_match(taggedText, "(\"|\\+)([가-힣]{2,4})(/ncps|/ncn|/pvg|/paa)") #상태명사, 비서술명사, 일반동사, 성상형용사 # 단어 길이 2~3 안전
+  attrs <- unique(na.omit(attrs[,3]))
+  basketline <- paste(attrs, collapse = ",")
+  print(text)
+  #print(taggedText) #디버깅용
+  print(basketline)
+  return(basketline)
 }
 
-unique(unlist(strsplit(PosiNega(posi[1:100]), " ")))
-
-P <- function(doc){
-  #doc <- as.character(doc)
-  doc2 <- paste(SimplePos22(doc))
-  doc3 <- str_match(doc2, "([가-힣]+)/PV")
-  doc4 <- doc3[,2]
-  doc4[!is.na(doc4)]
-  return(doc4[!is.na(doc4)])
-}
-
-
-PosiNega <- function(vec){
-  ret <- c()
-  i <- 0
-  for(line in vec){
-    print(paste(SimplePos22(line)))
-    nc <- NC(line)
-    #print("NCCCCCC")
-    #print(nc)
-    #print("PPPPPPP")
-    p <- P(line)
-    #print(p)
-    ret <- c(ret, nc[str_length(nc) > 1], p[str_length(p) > 1])
-    
-    print("------------")
-    i <- i + 1
-  }
-  return(ret)
-}
-a <- c("난", "가난 ", "배고", " ")
-
-
-posi <- readLines("neg_pol_word.txt")
-
-test <- unique(unlist(strsplit(PosiNega(posi[1:9826]), " ")))
-class(PosiNega(str_split("나는 매우 가난하다 그런데 ..", " ")))
-
-
-basket.str <- c()
+#textToTags("데이터 마이닝 좋다. 텍스트 마이닝 좋다.") #테스트를 하는 곳
+####################### basket formatting ###############################
 time.start <- Sys.time()
-for(i in 1:nrow(df)){
-  basket.line <- paste(unique(unlist(strsplit(PosiNega(df$content[i]), " "))), collapse = ",")
-  df$basket[i] <- basket.line
+file <- "basket.txt"
+if (file.exists(file)) file.remove(file)
+for(text in df$content){
+  write.table(textToBasket(text), file = "basket.txt", append = T, row.names = F, col.names = F)
 }
 time.end <- Sys.time()
 
-# data1 <- df$content[1:10]
-# data2 <- Map(extractNoun, txt1)
-# data2
-# rapply(data2, function(x) gsub())
+(basket <- read.table(file = "basket.txt", strip.white = T))
 
-#--------trash
-#Quotes_Eliminator <- function(x){
-#  return(substr(x,3,6))
-#}
-#df[2:nrow(df),1] = sapply(df[2:nrow(df),1],Quotes_Eliminator)
-#df<-df[,2:(ncol(df)-1)]
-#df
-#colnames(df)<-c("search_time","class","negative_word","keyword1","keyword2","channel","source","source_url","title","content")
+posi <- readLines("neg_pol_words.txt")
+test <- unique(unlist(strsplit(PosiNega(posi[1:9826]), " ")))
 
-#con <- file('NamYang_Data_Table.csv',encoding="UTF-16")
-#write.csv(df,file=con)
-#write.xlsx(df,file="NY_Data_Refined.xlsx")
+###################### pos tagging 연습 ##########################
+library(stringr)
+#(tagged <- paste(SimplePos22(df$content[1])))
+(tagged <- paste(MorphAnalyzer(df$content[1]))) #1:100
+##unique(str_match(tagged, "([가-힣]+)/ncp")[,2]) #서술성 명사
+##unique(str_match(tagged, "([가-힣]+)/ncpa")[,2]) #동작성 명사 .. 주말여행, 매매, 저축, 운전, 수원, 감수, 구도, 경주, 상승, 적용, 소방, 양보, 한잔, 출산
+(noun <- str_match(tagged, "([가-힣]+)/ncps")[,2]) #상태성 명사 .. 특별, 따듯, 지난, 유한, 상이, 불구, 자유, 사사
+(noun <- str_match(tagged, "([가-힣]+)/ncn")[,2]) #비서술성 명사 .. 중학생, 현대아파트, 충남, 인간, 농촌
+##unique(str_match(tagged, "([가-힣]+)/ncr")[,2]) #직위 명사 .. 수상, 경찰관, 고문, 임원, 신부, 교장, 기자
+##unique(str_match(tagged, "([가-힣]+)/pvd")[,2]) #지시 동사 .. NA
+(verb <- str_match(tagged, "([가-힣]+)/pvg")[,2]) #일반 동사 .. 훔치, 즐기, 비오, 먹, 막히, 건너, 소문나, 입, 오르
+##unique(str_match(tagged, "([가-힣]+)/pad")[,2]) #지시형용사 .. 있다, 기다
+(adjective <- str_match(tagged, "([가-힣]+)/paa")[,2]) #성상형용사 .. 좋, 잘, 달, 잘생기, 아름답, 새롭, 춥, 쌀쌀하, 드넓, 가볍
+#unique(str_match(tagged, "([가-힣]+)/px")[,2]) #보조용언 .. 내, 받, 않, 말, 줄, 두, 대, 들
+
+###################### 정규식 연습 ##########################
+str_match(c("12a", "123b", "1234a", "12345b"), "^([0-9]{2,3})(a|b)$")
+str_match(c("가나다a", "가나다라b", "가나다/paa", "가나/pvg"), "([가-힣]{2,3})(/paa|b|/pvg)")
+str_match(tagged, "([가-힣]{1,3})(/ncps|/pvg|/paa)")
+
+(taggedText <- paste(MorphAnalyzer(df$content[2])))
+(taggedWord <- str_match(taggedText, "(\"|\\+)(([가-힣]{2,4}/(ncps|ncn))|([가-힣]{1,3}/(pvg|paa)))"))
+(na.omit(taggedWord))
+
+
+
